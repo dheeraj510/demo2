75545840e243ad699af4c25476c83807266cb34b
Dheeraj <dheeru.manthani@gmail.com>
2013-11-16 18:51:38 +0530

    updated demo2 16th saturdey

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Demo2::Application.routes.draw do
   resources :posts
 
   devise_for :users
+  
+  root :to => 'posts#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +56,7 @@ Demo2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
